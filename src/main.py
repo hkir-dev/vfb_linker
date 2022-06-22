e5f2b50beb62a125a86eb11d5f485e10840a0d9b
hkir-dev <huseyin_kir@yahoo.com>
2022-06-22 12:09:59 +0100

    pmc processins

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import os
+
+from pmc_utils import read_csv_to_dict, write_mentions_to_file, clean_folder
+
 import spacy
 import scispacy
 from scispacy.linking import EntityLinker
-
 from scispacy.linking_utils import KnowledgeBase
 from scispacy.candidate_generation import DEFAULT_PATHS, DEFAULT_KNOWLEDGE_BASES
 from scispacy.candidate_generation import (
@@ -10,6 +12,9 @@ from scispacy.candidate_generation import (
     LinkerPaths
 )
 
+LINKING_FILE_EXTENSION = "_linking.tsv"
+DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data")
+OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../output")
 
 FBBT_JSON = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../resources/fbbt-cedar.jsonl")
 nmslib_index = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../linker/nmslib_index.bin")
@@ -41,25 +46,113 @@ DEFAULT_KNOWLEDGE_BASES["fbbt"] = FBBTKnowledgeBase
 
 linker = CandidateGenerator(name="fbbt")
 
-nlp = spacy.load("en_core_sci_sm")
-nlp.add_pipe("scispacy_linker", config={"resolve_abbreviations": True, "linker_name": "fbbt", "threshold": 0.85})
 
-sentence = "The metameric furrows and MesEc that forms between segments during embryonic stage 11 and persists to the " \
-           "larval stage (Campos-Ortega and Hartenstein, 1985). Any tracheal lateral trunk anterior branch primordium " \
-           "(FBbt:00000234) that is part of some metathoracic tracheal primordium (FBbt:00000188)."
-doc = nlp(sentence)
-# doc = nlp("Spinal and bulbar muscular atrophy (SBMA) is an \
-#            inherited motor neuron disease caused by the expansion \
-#            of a polyglutamine tract within the androgen receptor (AR). \
-#            SBMA can be caused by this easily.")
+def main():
+    """
+    Loads the pre-trained embedding model and processes pmc files existing in the data folder. As an output generates
+    new entity linking tables in the data folder.
+    """
+    nlp = spacy.load("en_core_sci_sm")
+    nlp.add_pipe("scispacy_linker", config={"resolve_abbreviations": True, "linker_name": "fbbt", "threshold": 0.85})
 
-words = sentence.split(" ")
-for ent in doc.ents:
-    # if ent._.kb_ents:
-        print("Mention : " + ent.text)
-        for fbbt_ent in ent._.kb_ents:
-            print("----------")
-            print(fbbt_ent)
-            print(linker.kb.cui_to_entity[fbbt_ent[0]])
-        print("********")
+    # process_test_sentence(nlp)
+    process_data_files(nlp)
 
+
+def process_test_sentence(nlp):
+    """
+    Runs entity linking on a basic test sentence.
+    :param nlp: embedding model
+    :return:
+    """
+    sentence = "The metameric furrows and MesEc that forms between segments during embryonic stage 11 and persists to the " \
+               "larval stage (Campos-Ortega and Hartenstein, 1985). Any tracheal lateral trunk anterior branch primordium " \
+               "(FBbt:00000234) that is part of some metathoracic tracheal primordium (FBbt:00000188)."
+    mentions = process_sentence(nlp, sentence)
+    for mention in mentions:
+        print(mention)
+
+
+def process_data_files(nlp):
+    """
+    Processes all files in the data folder and generates result tables.
+    :param nlp: entity linking model.
+    """
+    clean_folder(OUTPUT_FOLDER)
+    data_files = sorted(os.listdir(DATA_FOLDER))
+    for filename in data_files:
+        file_path = os.path.join(DATA_FOLDER, filename)
+        if os.path.isfile(file_path) and LINKING_FILE_EXTENSION not in filename:
+            table = read_csv_to_dict(file_path, delimiter="\t", generated_ids=True)[1]
+            all_mentions = list()
+            for row in table:
+                record = table[row]
+                mentions = process_sentence(nlp, record["text"])
+                for mention in mentions:
+                    mention["file_name"] = str(filename).split(".")[0].split("_")[0]
+                    if "section" in record:
+                        mention["section"] = record["section"]
+                    elif "tag" in record:
+                        mention["section"] = record["tag"]
+                    if "paragraph" in record:
+                        mention["paragraph"] = record["paragraph"]
+                    elif "label" in record:
+                        mention["paragraph"] = record["label"]
+                    if "sentence" in record:
+                        mention["sentence_num"] = record["sentence"]
+
+                    all_mentions.append(mention)
+            output_path = str(file_path).replace("/data/", "/output/")
+            output_path = output_path.replace("_captions", "")
+            write_mentions_to_file(output_path, all_mentions, append=True)
+
+
+def is_already_mentioned(mentions, text):
+    """
+    Checks if text is already mentioned in the same sentence.
+    :param mentions: list of existing mentions.
+    :param text: new mention to evaluate
+    :return: True id already mentioned, False otherwise
+    """
+    for mention in mentions:
+        if mention["mention_text"] == text:
+            return True
+    return False
+
+
+def process_sentence(nlp, sentence):
+    """
+    Processes a sentence to link entities.
+    :param nlp: embedding model
+    :param sentence: sentence to process
+    :return: list of mentions (entity linking results)
+    """
+    doc = nlp(sentence)
+    mentions = list()
+    for ent in doc.ents:
+        if ent._.kb_ents:
+            entity = ent._.kb_ents[0]
+            entity_id = entity[0]
+            confidence = entity[1]
+            linking = linker.kb.cui_to_entity[entity_id]
+            if not is_already_mentioned(mentions, ent.text):
+                mentions.append({
+                    "mention_text": ent.text,
+                    "sentence": sentence,
+                    "candidate_entity_iri": entity_id,
+                    "candidate_entity_label": linking.canonical_name,
+                    "candidate_entity_aliases": ",".join(linking.aliases),
+                    "confidence": confidence
+                })
+            # print("Mention : " + ent.text)
+            # for fbbt_ent in ent._.kb_ents:
+            #     print("----------")
+            #     print(fbbt_ent)
+            #     linking = linker.kb.cui_to_entity[fbbt_ent[0]]
+            #     print(linker.kb.cui_to_entity[fbbt_ent[0]])
+            # print("********")
+    return mentions
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pmc_utils.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import csv
+import pandas as pd
+
+
+def read_tsv_to_dict(tsv_path, id_column=0):
+    """
+    Reads tsv file content into a dict. Key is the first column value and the value is dict representation of the
+    row values (each header is a key and column value is the value).
+    Args:
+        tsv_path: Path of the TSV file
+        id_column: Id column becomes the key of the dict. This column should be unique. Default value is first column.
+    Returns:
+        Function provides two return values: first; headers of the table and second; the TSV content dict. Key of the
+        content is the first column value and the values are dict of row values.
+    """
+    return read_csv_to_dict(tsv_path, id_column=id_column, delimiter="\t")
+
+
+def read_csv_to_dict(csv_path, id_column=0, id_column_name="", delimiter=",", id_to_lower=False, generated_ids=False):
+    """
+    Reads tsv file content into a dict. Key is the first column value and the value is dict representation of the
+    row values (each header is a key and column value is the value).
+    Args:
+        csv_path: Path of the CSV file
+        id_column: Id column becomes the keys of the dict. This column should be unique. Default is the first column.
+        id_column_name: Alternative to the numeric id_column, id_column_name specifies id_column by its header string.
+        delimiter: Value delimiter. Default is comma.
+        id_to_lower: applies string lowercase operation to the key
+        generated_ids: If 'True', uses row number as the key of the dict. Initial key is 0.
+
+    Returns:
+        Function provides two return values: first; headers of the table and second; the CSV content dict. Key of the
+        content is the first column value and the values are dict of row values.
+    """
+    records = dict()
+
+    headers = []
+    with open(csv_path) as fd:
+        rd = csv.reader(fd, delimiter=delimiter, quotechar='"')
+        row_count = 0
+        for row in rd:
+            _id = row[id_column]
+            if id_to_lower:
+                _id = str(_id).lower()
+            if generated_ids:
+                _id = row_count
+
+            if row_count == 0:
+                headers = row
+                if id_column_name and id_column_name in headers:
+                    id_column = headers.index(id_column_name)
+            else:
+                row_object = dict()
+                for column_num, column_value in enumerate(row):
+                    row_object[headers[column_num]] = column_value
+                records[_id] = row_object
+
+            row_count += 1
+
+    return headers, records
+
+
+def write_mentions_to_file(file_path, mentions, append=False):
+    add_headers = True
+    if append:
+        mode = "a"
+        if os.path.isfile(file_path):
+            add_headers = False
+    else:
+        mode = "w"
+
+    class_robot_template = pd.DataFrame.from_records(mentions)
+    class_robot_template.to_csv(file_path, sep="\t", index=False, mode=mode, header=add_headers,
+                                columns=["file_name", "section", "paragraph", "sentence_num", "mention_text", "candidate_entity_iri", "candidate_entity_label", "candidate_entity_aliases", "confidence", "sentence"])
+
+
+def clean_folder(folder):
+    """
+    Cleans the content of the given folder.
+    :param folder: cleans the content of the output folder
+    """
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
